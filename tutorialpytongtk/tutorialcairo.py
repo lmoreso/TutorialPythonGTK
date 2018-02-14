3fc3cce9428e2180fe43197bc7c2a4d08958e2b0
Lluis Moreso Bosch <lmoreso@yahoo.com>
2018-02-14 09:31:38 +0100

    Cairo: eliminar orden antes de otras acciones

--- tutorialpytongtk/tutorialcairo.py
+++ tutorialpytongtk/tutorialcairo.py
@@ -4,6 +4,7 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 import figurasgeo
+import random
 
 fitxer_glade = "TutorialCairo.glade"
 main_window_name = "win_ex_cairo"
@@ -66,38 +67,51 @@ class CairoWindowHandler:
     def on_col_desc_edited(self, widget, path, text):
         self.LstStorePunts[path][2] = text    
         
+    def elimina_orden(self):
+        self.LstStorePunts.set_sort_column_id(Gtk.TREE_SORTABLE_UNSORTED_SORT_COLUMN_ID, 0)
+
+    def on_btn_cairo_unsort(self, param):
+        self.elimina_orden()
+        
     def on_btn_recta(self, param):
         print("on_btn_recta")
+        self.elimina_orden()
         figurasgeo.Recta(self.LstStorePunts)
         self.LstSelection.select_iter(self.LstStorePunts.get_iter_first())
             
     def on_btn_bezier(self, param):
         print("on_btn_bezier")
+        self.elimina_orden()
         figurasgeo.Bezier(self.LstStorePunts)
         self.LstSelection.select_iter(self.LstStorePunts.get_iter_first())
             
     def on_btn_logaritm(self, param):
         print("on_btn_logaritm", param)
+        self.elimina_orden()
         figurasgeo.Logaritm(self.LstStorePunts, 2, 1, -8, 3)
         self.LstSelection.select_iter(self.LstStorePunts.get_iter_first())
     
     def on_btn_elipse(self, param):
         print("on_btn_elipse", param)
+        self.elimina_orden()
         figurasgeo.Elipse(self.LstStorePunts, 5, 4)
         self.LstSelection.select_iter(self.LstStorePunts.get_iter_first())
             
     def on_btn_sinus(self, param):
         print("on_btn_sinus", param)
+        self.elimina_orden()
         figurasgeo.Sinus(self.LstStorePunts, False, 5, 0.05)
         self.LstSelection.select_iter(self.LstStorePunts.get_iter_first())
         
     def on_btn_parabola(self, param):
         print("on_btn_parabola", param)
+        self.elimina_orden()
         figurasgeo.Parabola(self.LstStorePunts, 0.40, 0, -5.8, 0.25)
         self.LstSelection.select_iter(self.LstStorePunts.get_iter_first())
 
     def on_btn_hiperbola(self, param):
         print("on_btn_hiperbola", param)
+        self.elimina_orden()
         figurasgeo.Hiperbola(self.LstStorePunts, 1, 0)
         self.LstSelection.select_iter(self.LstStorePunts.get_iter_first())
            
@@ -203,6 +217,7 @@ class CairoWindowHandler:
         print("cb_btn_superior", b)
         model, listiter  = self.LstSelection.get_selected()
         if listiter != None:
+            self.elimina_orden()
             listiter_prev = self.LstStorePunts.iter_previous(listiter)
             if listiter_prev != None:
                 self.LstStorePunts.swap(listiter, listiter_prev)
@@ -219,6 +234,7 @@ class CairoWindowHandler:
         print("cb_btn_amunt", b)
         model, listiter  = self.LstSelection.get_selected()
         if listiter != None:
+            self.elimina_orden()
             self.LstStorePunts.move_after(listiter)
             print("  Mogut: ", model[listiter][:])                      
             self.print_punts("".ljust(2))
@@ -229,6 +245,7 @@ class CairoWindowHandler:
     
     def cb_btn_add(self, b):
         print("cb_btn_add", b)
+        self.elimina_orden()
         x = random.randrange(-50, 50, 1) / 10.0
         y = random.randrange(-50, 50, 1) / 10.0
         listiter = self.LstStorePunts.append([x, y, "afegit!"])
@@ -242,6 +259,7 @@ class CairoWindowHandler:
         print("cb_btn_delete", b)
         model, listiter  = self.LstSelection.get_selected()
         if listiter != None:
+            self.elimina_orden()
             print("".ljust(2), "A Borrar: ", self.LstStorePunts[listiter][:])          
             self.LstStorePunts.remove(listiter)
             self.print_punts("".ljust(2))
@@ -254,6 +272,7 @@ class CairoWindowHandler:
         print("cb_btn_avall", b)
         model, listiter  = self.LstSelection.get_selected()
         if listiter != None:
+            self.elimina_orden()
             listiter_next = self.LstStorePunts.iter_next(listiter)
             if listiter_next != None:
                 self.LstStorePunts.swap(listiter, listiter_next)
@@ -270,7 +289,7 @@ class CairoWindowHandler:
         print("cb_btn_inferior", b)
         model, listiter  = self.LstSelection.get_selected()
         if listiter != None:
-            #print(listiter.stamp, listiter.user_data, listiter.user_data2, listiter.user_data3)
+            self.elimina_orden()
             self.LstStorePunts.move_before(listiter)
             print("".ljust(2), "Mogut: ", model[listiter][:])                      
             self.print_punts("".ljust(2))
@@ -294,6 +313,7 @@ class CairoWindow:
             mainwindow.set_modal(True)
         mainwindow.show_all()
         if bEsMainApp:
+            builder.get_object("btn_cairo_cerrar").hide()
             Gtk.main()
 
 if __name__ == "__main__":
